28130e6f834a5ae52cef790906817000566c911d
elmaaroufi said <saidoxcasa17@gmail.com>
2026-02-28 14:56:48 +0000

    affichage des produits apartir d'un tableau

---
+++ index.js
@@ -0,0 +1,60 @@
+const products = [
+    {nom : 'pain de mie complet', code : '20400571'},
+    {nom : 'pain de mie nature', code : '20400572'},
+]
+
+const  pdtContainer = document.getElementById('showPdt');
+
+
+const inputProduct = document.getElementById('pdt');
+
+let pdtX = '';
+
+
+inputProduct.addEventListener('input' , (event) => {
+    let inputValue =  event.target.value; //20
+
+    if(inputValue == ''){
+        pdtContainer.textContent = ''; 
+    }else{
+        console.log(typeof(pdtX))
+        if(pdtX == ''){
+
+            pdtX = products.filter((pdt) => pdt.nom.startsWith(inputValue) || pdt.code.startsWith(inputValue) ? pdt : '')
+            if(pdtX == ''){
+                return
+            }else{
+                
+                pdtX.map((pdt) => {
+                    const tr = document.createElement('tr');
+                    const tdNom = document.createElement('td');
+                    const tdCode = document.createElement('td');
+                    
+                    tdNom.textContent = pdt.nom;
+                    tdCode.textContent = pdt.code;
+                    
+                    tr.appendChild(tdNom);
+                    tr.appendChild(tdCode);
+                    pdtContainer.appendChild(tr) ;
+                })
+            }
+        }else{
+            pdtContainer.textContent = '';
+            pdtX.map((pdt) => {
+                    const tr = document.createElement('tr');
+                    const tdNom = document.createElement('td');
+                    const tdCode = document.createElement('td');
+                    
+                    tdNom.textContent = pdt.nom;
+                    tdCode.textContent = pdt.code;
+                    
+                    tr.appendChild(tdNom);
+                    tr.appendChild(tdCode);
+                    pdtContainer.appendChild(tr) ;
+        }
+    )
+    }
+}
+            
+})           
+  
