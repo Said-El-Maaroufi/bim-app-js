4ba9f3023f30201a0c6f458a7c9401921ee58027
elmaaroufi said <saidoxcasa17@gmail.com>
2026-03-01 01:05:16 +0000

    the search by name and code is working with the function filter on the same table

--- index.js
+++ index.js
@@ -10,39 +10,72 @@ const inputProduct = document.getElementById('pdt');
 
 let pdtX = '';
 
+console.log(pdtX)
+
+
+let x = 20400571;
+let y = '20400571'
+console.log(y.startsWith(x))
+console.log(x)
 
 inputProduct.addEventListener('input' , (event) => {
     let inputValue =  event.target.value; //20
 
     if(inputValue == ''){
-        pdtContainer.textContent = ''; 
+        pdtContainer.textContent = '';
         return
     }else{
-
+//2040057
+        if(inputValue.toString().length >= 2 ){
             pdtX = products.filter((pdt) => pdt.nom.startsWith(inputValue) || pdt.code.startsWith(inputValue) ? pdt : '')
-            if(pdtX == ''){
-                return
-            }else{
-                
-                if(pdtX.length == 1){
-                        pdtX.map((pdt) => {
+            pdtContainer.textContent = '';
+                pdtX.map((pdt) => {
                             const tr = document.createElement('tr');
                             const tdNom = document.createElement('td');
                             const tdCode = document.createElement('td');
-
+                            
                             tdNom.textContent = pdt.nom;
                             tdCode.textContent = pdt.code;
-
-
+                            
+                            
                             tr.appendChild(tdNom);
                             tr.appendChild(tdCode);
                             pdtContainer.appendChild(tr);
+                            
                         })
-                    }else{
-                pdtX = pdtX.filter((pdt) => pdt.nom.startsWith(inputValue) || pdt.code.startsWith(inputValue) ? pdt : '')
-                    }
+            }else{
+
+                
+                
+                pdtX = products.filter((pdt) => pdt.nom.startsWith(inputValue) || pdt.code.startsWith(inputValue) ? pdt : '')
+                
+                pdtX.map((pdt) => {
+                    const tr = document.createElement('tr');
+                    const tdNom = document.createElement('td');
+                    const tdCode = document.createElement('td');
+                    
+                    tdNom.textContent = pdt.nom;
+                    tdCode.textContent = pdt.code;
+                    
+                    
+                    tr.appendChild(tdNom);
+                    tr.appendChild(tdCode);
+                    pdtContainer.appendChild(tr);
+                    
+                })
+                // for (let i = 0; index < pdtX.length; i++) {
+                }
             }
-}
-}
+                    
+                //     pdtX = pdtX.filter((pdt) => pdt.nom.startsWith(inputValue) || pdt.code.startsWith(inputValue) ? pdt : '')
+                    
+                //     if(pdtX.length == 1){
+                //        
+                //     }
+                //     }w
+                    
+            }
+
+
 
 )
