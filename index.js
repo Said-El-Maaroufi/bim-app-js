3b9608bd004b3d27bd07e6da5faa0687a578fc00
elmaaroufi said <saidoxcasa17@gmail.com>
2026-03-01 15:12:19 +0000

    add some attributes to the product object

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const products = [
-    {nom : 'pain de mie complet', code : '20400571'},
-    {nom : 'pain de mie nature', code : '20400572'},
+    {nom : 'EAU VITALYA 5L', code : '21704398', bareCode:'', image:''},
+    {nom : 'PACK EAU-MINRA 5l+1', code : '21706062' , bareCode:'', image:''},
+    {nom : 'EAU MINERAL 5+1SAISS', code : '21704937', bareCode:'', image:''},
+    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'AIN SAISS', code : '21704937', bareCode:'', image:'', prix:'12.80', Qte:'5L+1'},
+    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'SIDI ALI', code : '20300011', bareCode:'', image:'', prix:'', Qte:'0.5'},
 ]
 
 const  pdtContainer = document.getElementById('showPdt');
@@ -10,14 +13,6 @@ const inputProduct = document.getElementById('pdt');
 
 let pdtX = '';
 
-console.log(pdtX)
-
-
-let x = 20400571;
-let y = '20400571'
-console.log(y.startsWith(x))
-console.log(x)
-
 inputProduct.addEventListener('input' , (event) => {
     let inputValue =  event.target.value; //20
 
@@ -25,8 +20,6 @@ inputProduct.addEventListener('input' , (event) => {
         pdtContainer.textContent = '';
         return
     }else{
-//2040057
-        if(inputValue.toString().length >= 2 ){
             pdtX = products.filter((pdt) => pdt.nom.startsWith(inputValue) ? pdt : '')
             pdtContainer.textContent = '';
                 pdtX.map((pdt) => {
@@ -42,39 +35,9 @@ inputProduct.addEventListener('input' , (event) => {
                             tr.appendChild(tdCode);
                             pdtContainer.appendChild(tr);
                             
-                        })
-            }else{
-
-                
-                
-                pdtX = products.filter((pdt) => pdt.nom.startsWith(inputValue) ? pdt : '')
-                
-                pdtX.map((pdt) => {
-                    const tr = document.createElement('tr');
-                    const tdNom = document.createElement('td');
-                    const tdCode = document.createElement('td');
-                    
-                    tdNom.textContent = pdt.nom;
-                    tdCode.textContent = pdt.code;
-                    
-                    
-                    tr.appendChild(tdNom);
-                    tr.appendChild(tdCode);
-                    pdtContainer.appendChild(tr);
-                    
                 })
-                // for (let i = 0; index < pdtX.length; i++) {
-                }
             }
-                    
-                //     pdtX = pdtX.filter((pdt) => pdt.nom.startsWith(inputValue) || pdt.code.startsWith(inputValue) ? pdt : '')
-                    
-                //     if(pdtX.length == 1){
-                //        
-                //     }
-                //     }w
-                    
-            }
+        }
 
 
 
