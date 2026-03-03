0665421ff37e4c06d47539774833398965dc3672
elmaaroufi said <saidoxcasa17@gmail.com>
2026-03-03 02:01:56 +0000

    add a page call add for add new product & put the products table a page data.js for more organisation

---
+++ data.js
@@ -0,0 +1,18 @@
+
+// declaration table du produits
+export const products = [
+    {nomFrance : 'Eau de Table', nomArabe: '',marque:'VITALYA', code : '21704398', bareCode:'',prix:'9.90', image:'', Qte:'5.0'},
+    {nomFrance : 'PACK EAU-MINRA 5l+1', nomArabe : '',marque:'AIN SAISS', code : '21706062' , bareCode:'', image:'', prix: '23.90', Qte:'6x2'},
+    {nomFrance : 'EAU MINERAL 5+1SAISS',nomArabe:'',marque:'AIN SAISS', code : '21704937', bareCode:'', image:'', prix:'12.80', Qte:'5L+1'},
+    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'AIN SAISS', code : '20300857', bareCode:'', image:'', prix:'2.50', Qte:'0.5'},
+    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'AIN SAISS', code : '20300858', bareCode:'', image:'', prix:'4.55', Qte:'1.5'},
+    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'SIDI ALI', code : '20300011', bareCode:'', image:'', prix:'2.75', Qte:'0.5'},
+    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'SIDI ALI', code : '20300010', bareCode:'', image:'', prix:'4.80', Qte:'1.5'},
+    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'SIDI ALI', code : '20300174', bareCode:'', image:'', prix:'1.70', Qte:'0.33C'},
+    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'SIDI ALI', code : '20300562', bareCode:'', image:'', prix:'5.50', Qte:'2.0'},
+    {nomFrance : 'Eau de source naturelle', nomArabe:'', marque : 'ACYL', code : '21705244', bareCode:'', image:'', prix:'4.50', Qte:'1.5'},
+    {nomFrance : 'Eaux de table', nomArabe:'', marque : 'BAHIA', code : '20300009', bareCode:'', image:'', prix:'9.95', Qte:'5.0'},
+    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'AIN ATLAS', code : '20300085', bareCode:'', image:'', prix:'11.70', Qte:'5.0'},
+    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'AIN IFRANE', code : '21705828', bareCode:'', image:'', prix:'10.90', Qte:'5.0'},
+]
+
--- index.js
+++ index.js
@@ -1,29 +1,18 @@
-const products = [
-    {nomFrance : 'Eau de Table', nomArabe: '',marque:'VITALYA', code : '21704398', bareCode:'',prix:'9.90', image:'', Qte:'5.0'},
-    {nomFrance : 'PACK EAU-MINRA 5l+1', nomArabe : '',marque:'AIN SAISS', code : '21706062' , bareCode:'', image:'', prix: '23.90', Qte:'6x2'},
-    {nomFrance : 'EAU MINERAL 5+1SAISS',nomArabe:'',marque:'AIN SAISS', code : '21704937', bareCode:'', image:'', prix:'12.80', Qte:'5L+1'},
-    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'AIN SAISS', code : '20300857', bareCode:'', image:'', prix:'2.50', Qte:'0.5'},
-    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'AIN SAISS', code : '20300858', bareCode:'', image:'', prix:'4.55', Qte:'1.5'},
-    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'SIDI ALI', code : '20300011', bareCode:'', image:'', prix:'2.75', Qte:'0.5'},
-    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'SIDI ALI', code : '20300010', bareCode:'', image:'', prix:'4.80', Qte:'1.5'},
-    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'SIDI ALI', code : '20300174', bareCode:'', image:'', prix:'1.70', Qte:'0.33C'},
-    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'SIDI ALI', code : '20300562', bareCode:'', image:'', prix:'5.50', Qte:'2.0'},
-    {nomFrance : 'Eau de source naturelle', nomArabe:'', marque : 'ACYL', code : '21705244', bareCode:'', image:'', prix:'4.50', Qte:'1.5'},
-    {nomFrance : 'Eaux de table', nomArabe:'', marque : 'BAHIA', code : '20300009', bareCode:'', image:'', prix:'9.95', Qte:'5.0'},
-    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'AIN ATLAS', code : '20300085', bareCode:'', image:'', prix:'11.70', Qte:'5.0'},
-    {nomFrance : 'Eau minérale naturelle', nomArabe:'', marque : 'AIN IFRANE', code : '21705828', bareCode:'', image:'', prix:'10.90', Qte:'5.0'},
-]
 
-const  pdtContainer = document.getElementById('showPdt');
+// importation des produits
+import { products } from "./data.js";
 
-let x = 'vitalya 5l'
-let y = 'vitalya'
-x = x.split(' ') 
-console.log(x)
-console.log(y.startsWith(x[0]))
+console.log(products)
+// recuperer l'id ou d'input où il tape l'utilisateur
 const inputProduct = document.getElementById('pdt');
 
-let pdtX = '';
+// recuperer l'id ou l'on affiche les produits c'est une balise div
+const  pdtContainer = document.getElementById('showPdt');
+
+
+// declaration table vide pour le deuxieme filtre
+let pdtX = [];
+
 
 inputProduct.addEventListener('input' , (event) => {
     let inputValue =  event.target.value; //20
@@ -45,11 +34,13 @@ inputProduct.addEventListener('input' , (event) => {
         return
     }else{
             if(inputValue.toString().length >= 2){
-
+// '3 '
+// sidialideuxlitre
+// SIDI ALI
                 pdtX = pdtX.filter((pdt) => 
-                    pdt.nomFrance.toLowerCase().startsWith(inputValue.toLowerCase().split(' ')[0]) 
-                ||  pdt.marque.toLowerCase().startsWith(inputValue.toLowerCase().split(' ')[0])
-                ||  pdt.code.startsWith(inputValue)
+                    pdt.nomFrance.toLowerCase().includes(inputValue.toLowerCase().split('')[0]) 
+                ||  pdt.marque.toLowerCase().includes(inputValue.toLowerCase().split('')[0])
+                ||  pdt.code.includes(inputValue.toString().split('')[0])
                 ? pdt : ''
                 )
                 pdtContainer.textContent = '';
@@ -76,27 +67,32 @@ inputProduct.addEventListener('input' , (event) => {
 
             pdtX =
             products.filter((pdt) => 
-                pdt.nomFrance.toLowerCase().startsWith(inputValue.toLowerCase().split(' ')[0]) 
-            ||  pdt.marque.toLowerCase().startsWith(inputValue.toLowerCase().split(' ')[0])
+                pdt.nomFrance.toLowerCase().startsWith(inputValue.toLowerCase()) 
+            ||  pdt.marque.toLowerCase().startsWith(inputValue.toLowerCase())
             ||  pdt.code.startsWith(inputValue)
             ? pdt : '')
+            if(pdtX[0] == ''){
+                return
+            }else{
+
                 pdtX.map((pdt) => {
-                            const tr = document.createElement('tr');
-                            const tdNom = document.createElement('td');
-                            const tdCode = document.createElement('td');
-                            const tdQte = document.createElement('td')
-                            
-                            tdNom.textContent = pdt.nomFrance;
-                            tdCode.textContent = pdt.code;
-                            tdQte.textContent = pdt.Qte +'L'
-                            
-                            
-                            tr.appendChild(tdNom);
-                            tr.appendChild(tdCode);
-                            tr.appendChild(tdQte);
-                            pdtContainer.appendChild(tr);
-                            
+                    const tr = document.createElement('tr');
+                    const tdNom = document.createElement('td');
+                    const tdCode = document.createElement('td');
+                    const tdQte = document.createElement('td')
+                    
+                    tdNom.textContent = pdt.nomFrance;
+                    tdCode.textContent = pdt.code;
+                    tdQte.textContent = pdt.Qte +'L'
+                    
+                    
+                    tr.appendChild(tdNom);
+                    tr.appendChild(tdCode);
+                    tr.appendChild(tdQte);
+                    pdtContainer.appendChild(tr);
+                    
                 })
+            }
             }
             }
         }
